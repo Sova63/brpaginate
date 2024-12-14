87da77ecf05d7e6345ce5e16f4e3454a42c4cfba
Sova63 <osnova-vsego@yandex.ru>
2024-12-14 18:18:56 +0300

    Добавил класс в admin.py

--- brpaginate/mypaginate/admin.py
+++ brpaginate/mypaginate/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-from .models import *
-admin.site.register(Article)
+from .models import Article
+
+
+
+class ArticlelAdmin(admin.ModelAdmin):
+	list_display = ('title','content','created_at')
+	search_fields = ('title','content')
+	list_filter = ('title',)
+	ordering = ('title',)
+
+admin.site.register(Article,ArticlelAdmin)
